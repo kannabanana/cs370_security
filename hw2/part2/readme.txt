7f97104703b8c78a6a2dd060743b532f0d318dde
Sr Kanna <kannas@flip2.engr.oregonstate.edu>
2016-11-20 12:46:08 -0800

    part 4&5

---
+++ hw2/part2/readme.txt
@@ -0,0 +1,4 @@
+the randomstring.py file produces a random string based on a length from the command line and the outputfile
+
+To execute:
+python randomstring num outputfile.txt
--- hw2/part3/opensslex.c
+++ hw2/part3/opensslex.c
@@ -18,23 +18,23 @@ void printarray(char * p)
 //OUTPUT: array with random values
 char *randstring(size_t length) {
 
-    static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";        
-    char *randomString = NULL;
+	static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";        
+	char *randomString = NULL;
 	int n;
 
-    if (length) {
-        randomString = malloc(sizeof(char) * (length +1));
+	if (length) {
+		randomString = malloc(sizeof(char) * (length +1));
 
-        if (randomString) {            
-            for (n= 0;n < length;n++) {            
-                int key = rand() % (int)(sizeof(charset) -1);
-                randomString[n] = charset[key];
-            }
+		if (randomString) {            
+			for (n= 0;n < length;n++) {            
+				int key = rand() % (int)(sizeof(charset) -1);
+				randomString[n] = charset[key];
+			}
 
-            randomString[length] = '\0';
-        }
-    }
-    return randomString;
+			randomString[length] = '\0';
+		}
+	}
+	return randomString;
 }
 
 
@@ -43,45 +43,12 @@ char *randstring(size_t length) {
 void main(int argc,char *argv[])
 {
 
-	/*Values that will not be recycled*/
-
-	int count = 0;						//starting 
-	int flip = 0;
-
-	size_t len = 10;					//create random string
-	char * random_string = NULL;
-	
-	while(flip != 1)
-	{
-		random_string = randstring(len);
-		printarray(random_string);
-		random_string = NULL;
-		++count;
-		if (count == 10)
-		{
-			flip = 1;
-		}
-	}
-
-	if (flip == 1)
-	{	
-		printf ("the count is %d\n",count);
-	}
-
-
-
-	/* Values that will be recycled*/	
 	EVP_MD_CTX *mdctx;					//new one w/the value
 	const EVP_MD *md;					//the md
-
+	int md_len,i;
 	char mess1[] = "Test Message\n";
-	char mess2[] = "Hello World\n";
 	unsigned char md_value[EVP_MAX_MD_SIZE];		//char of md value
-	int md_len, i;						//len and initialization
 
-	printf("origional message 1%s\n",mess1);
-	printf("origional message 2%s\n",mess2);
-	
 	OpenSSL_add_all_algorithms();				//need to add this
 	if(!argv[1]) {
 		printf("Usage: mdtest digestname\n");
@@ -102,7 +69,6 @@ void main(int argc,char *argv[])
 	
 	EVP_DigestInit_ex(mdctx, md, NULL);			//initalize
 	EVP_DigestUpdate(mdctx, mess1, strlen(mess1));		//update str1
-	EVP_DigestUpdate(mdctx, mess2, strlen(mess2));		
 	EVP_DigestFinal_ex(mdctx, md_value, &md_len);
 	EVP_MD_CTX_destroy(mdctx);				//destroy it
 
@@ -111,6 +77,83 @@ void main(int argc,char *argv[])
 		printf("%02x", md_value[i]);
 	printf("\n");
 
+
+	/*Values that will not be recycled*/
+
+	int count = 0;						//starting 
+	int flip = 0;
+
+	size_t len = 10;					//create random string
+	char * random_string = NULL;
+
+//	while(flip != 1)
+//	{
+		random_string = randstring(len);
+		printarray(random_string);
+		random_string = NULL;
+		
+
+
+		/* Values that will be recycled*/	
+		EVP_MD_CTX *mdctx2;					//new one w/the value
+		const EVP_MD *md2;					//the md
+		unsigned char md_value2[EVP_MAX_MD_SIZE];		//char of md value
+		int md_len2;						//len and initialization
+
+
+
+		OpenSSL_add_all_algorithms();				//need to add this
+		if(!argv[1]) {
+			printf("Usage: mdtest digestname\n");
+			exit(1);
+		}
+
+
+		md2 = EVP_get_digestbyname(argv[1]);			//get the name
+
+		
+		if(!md2) {
+			printf("Unknown message digest %s\n", argv[1]);
+			exit(1);
+		}
+
+		printf ("got md2 value\n");
+		mdctx2 = EVP_MD_CTX_create();				//create
+		printf ("created\n");
+		EVP_DigestInit_ex(mdctx2, md2, NULL);			//initalize
+		printf ("init\n");
+		EVP_DigestUpdate(mdctx2, random_string, strlen(random_string));		//update str1
+		printf ("update\n");
+		EVP_DigestFinal_ex(mdctx2, md_value2, &md_len2);
+		printf ("final");
+
+		//get first 26 bits of md_value and copy it into 
+	
+		EVP_MD_CTX_destroy(mdctx2);				//destroy it
+		printf("destroy\n");
+	
+
+		printf("Digest is: ");
+		for (i = 0; i < md_len2; i++)
+			printf("%02x", md_value2[i]);
+		printf("\n");
+
+
+/*
+
+		++count;
+		if (count == 10)
+		{
+			flip = 1;
+		}
+	}
+
+	if (flip == 1)
+	{	
+		printf ("the count is %d\n",count);
+	}
+
+*/
 	exit(0);
 
 }
